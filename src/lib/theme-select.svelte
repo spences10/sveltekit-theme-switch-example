c1b9223defc58da1a470e64f08e0a31d0a4d90ac
spences10 <spences10apps@gmail.com>
2021-07-04 17:26:32 +0100

    feat: :sparkles: move switch to own component

---
+++ src/lib/theme-select.svelte
@@ -0,0 +1,28 @@
+<div class="mt-5 mb-8">
+  <select
+    data-choose-theme
+    class="select select-bordered select-primary w-full max-w-xs"
+  >
+    <option value="light">Default</option>
+    <option value="dark">dark</option>
+    <option value="cupcake">cupcake</option>
+    <option value="bumblebee">bumblebee</option>
+    <option value="emerald">emerald</option>
+    <option value="corporate">corporate</option>
+    <option value="synthwave">synthwave</option>
+    <option value="retro">retro</option>
+    <option value="cyberpunk">cyberpunk</option>
+    <option value="valentine">valentine</option>
+    <option value="halloween">halloween</option>
+    <option value="garden">garden</option>
+    <option value="forest">forest</option>
+    <option value="aqua">aqua</option>
+    <option value="lofi">lofi</option>
+    <option value="pastel">pastel</option>
+    <option value="fantasy">fantasy</option>
+    <option value="wireframe">wireframe</option>
+    <option value="black">black</option>
+    <option value="luxury">luxury</option>
+    <option value="dracula">dracula</option>
+  </select>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import ThemeSelect from '$lib/theme-select.svelte'
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
   import '../app.postcss'
@@ -8,33 +9,9 @@
   })
 </script>
 
-<select
-  data-choose-theme
-  class="select select-bordered select-primary w-full max-w-xs"
->
-  <option value="light">Default</option>
-  <option value="dark">dark</option>
-  <option value="cupcake">cupcake</option>
-  <option value="bumblebee">bumblebee</option>
-  <option value="emerald">emerald</option>
-  <option value="corporate">corporate</option>
-  <option value="synthwave">synthwave</option>
-  <option value="retro">retro</option>
-  <option value="cyberpunk">cyberpunk</option>
-  <option value="valentine">valentine</option>
-  <option value="halloween">halloween</option>
-  <option value="garden">garden</option>
-  <option value="forest">forest</option>
-  <option value="aqua">aqua</option>
-  <option value="lofi">lofi</option>
-  <option value="pastel">pastel</option>
-  <option value="fantasy">fantasy</option>
-  <option value="wireframe">wireframe</option>
-  <option value="black">black</option>
-  <option value="luxury">luxury</option>
-  <option value="dracula">dracula</option>
-</select>
-
-<main>
-  <slot />
+<main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  <div class="max-w-3xl mx-auto">
+    <ThemeSelect />
+    <slot />
+  </div>
 </main>
