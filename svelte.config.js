d5a7c7a70244cc09c10a4909be9fae07f3faac3e
spences10 <spences10apps@gmail.com>
2021-07-04 17:28:23 +0100

    feat: :wrench: add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
+import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: adapter(),
   },
 
   preprocess: [
