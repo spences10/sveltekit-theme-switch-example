b30cb66cf2c48030b11d42e910dbcdf760db1014
Scott Spence <spences10apps@gmail.com>
2022-02-06 20:20:00 +0000

    Remove config.kit.target

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import preprocess from 'svelte-preprocess'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: adapter(),
   },
 
