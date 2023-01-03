ce3d974cf742078ae274725905026702441d566e
spences10 <spences10apps@gmail.com>
2023-01-03 08:55:57 +0000

    refactor: :recycle: update to latest SvelteKit template

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel'
-import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter(),
   },
-
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
 }
 
 export default config
-// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
-process.env.TAILWIND_MODE = dev ? 'watch' : 'build'
