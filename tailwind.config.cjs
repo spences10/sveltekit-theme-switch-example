9449e82ec09ec743510759e043f3d67fbfd98d1c
spences10 <spences10apps@gmail.com>
2023-06-11 16:44:30 +0100

    refactor: :recycle: include daisyUI v3 themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,14 @@
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+	},
+
+	daisyui: {
+		themes: true,
+	},
+
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
 
 module.exports = config
