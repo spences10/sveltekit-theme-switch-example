3ca311f13b762f8549d5992debc1d71c7adedec7
spences10 <spences10apps@gmail.com>
2023-06-18 19:26:56 +0100

    refactor: :recycle: themes into array

--- src/lib/theme-select.svelte
+++ src/lib/theme-select.svelte
@@ -1,37 +1,45 @@
+<script lang="ts">
+	const themes = [
+		'acid',
+		'aqua',
+		'autumn',
+		'black',
+		'bumblebee',
+		'business',
+		'cmyk',
+		'coffee',
+		'corporate',
+		'cupcake',
+		'cyberpunk',
+		'dark',
+		'dracula',
+		'emerald',
+		'fantasy',
+		'forest',
+		'garden',
+		'halloween',
+		'lemonade',
+		'light',
+		'lofi',
+		'luxury',
+		'night',
+		'pastel',
+		'retro',
+		'synthwave',
+		'valentine',
+		'winter',
+		'wireframe',
+	]
+</script>
+
 <div class="mb-8">
 	<select
 		data-choose-theme
-		class="select select-bordered select-primary w-full max-w-3xl text-xl"
+		class="select select-bordered select-primary w-full max-w-3xl text-xl capitalize"
 	>
 		<option disabled selected>Choose a theme</option>
-		<option value="acid">Acid</option>
-		<option value="aqua">Aqua</option>
-		<option value="autumn">Autumn</option>
-		<option value="black">Black</option>
-		<option value="bumblebee">Bumblebee</option>
-		<option value="business">Business</option>
-		<option value="cmyk">CMYK</option>
-		<option value="coffee">Coffee</option>
-		<option value="corporate">Corporate</option>
-		<option value="cupcake">Cupcake</option>
-		<option value="cyberpunk">Cyberpunk</option>
-		<option value="dark">Dark</option>
-		<option value="dracula">Dracula</option>
-		<option value="emerald">Emerald</option>
-		<option value="fantasy">Fantasy</option>
-		<option value="forest">Forest</option>
-		<option value="garden">Garden</option>
-		<option value="halloween">Halloween</option>
-		<option value="lemonade">Lemonade</option>
-		<option value="light">Light</option>
-		<option value="lofi">Lofi</option>
-		<option value="luxury">Luxury</option>
-		<option value="night">Night</option>
-		<option value="pastel">Pastel</option>
-		<option value="retro">Retro</option>
-		<option value="synthwave">Synthwave</option>
-		<option value="valentine">Valentine</option>
-		<option value="winter">Winter</option>
-		<option value="wireframe">Wireframe</option>
+		{#each themes as theme}
+			<option value={theme} class="capitalize">{theme}</option>
+		{/each}
 	</select>
 </div>
