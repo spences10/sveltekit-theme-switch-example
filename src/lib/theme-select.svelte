ef756d3956dc03e57ff5a46a37e89d812f3db8f5
spences10 <spences10apps@gmail.com>
2023-06-26 20:35:54 +0100

    refactor: :recycle: set current theme in select if there

--- src/lib/theme-select.svelte
+++ src/lib/theme-select.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte'
 	import { themes } from './themes'
 
-	let current_theme = '';
+	let current_theme = ''
 
 	onMount(() => {
 		if (typeof window !== 'undefined') {
-			const theme = window.localStorage.getItem('theme');
+			const theme = window.localStorage.getItem('theme')
 			if (theme && themes.includes(theme)) {
-				document.documentElement.setAttribute('data-theme', theme);
-				current_theme = theme;
+				document.documentElement.setAttribute('data-theme', theme)
+				current_theme = theme
 			}
 		}
 	})
@@ -19,16 +19,17 @@
 		const theme = select.value
 		if (themes.includes(theme)) {
 			const one_year = 60 * 60 * 24 * 365
-			window.localStorage.setItem('theme', theme);
+			window.localStorage.setItem('theme', theme)
 			document.cookie = `theme=${theme}; max-age=${one_year}; path=/`
 			document.documentElement.setAttribute('data-theme', theme)
-			current_theme = theme;
+			current_theme = theme
 		}
 	}
 </script>
 
 <div class="mb-8">
-	<select bind:value={current_theme}
+	<select
+		bind:value={current_theme}
 		data-choose-theme
 		class="select select-bordered select-primary w-full max-w-3xl text-xl capitalize"
 		on:change={set_theme}
