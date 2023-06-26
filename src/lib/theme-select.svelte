89ad1569347c595ecfe1ddd07a1fcdb356117458
spences10 <spences10apps@gmail.com>
2023-06-26 20:35:06 +0100

    refactor: :recycle: set current theme in select if there

--- src/lib/theme-select.svelte
+++ src/lib/theme-select.svelte
@@ -1,24 +1,39 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import { themes } from './themes'
+
+	let current_theme = '';
+
+	onMount(() => {
+		if (typeof window !== 'undefined') {
+			const theme = window.localStorage.getItem('theme');
+			if (theme && themes.includes(theme)) {
+				document.documentElement.setAttribute('data-theme', theme);
+				current_theme = theme;
+			}
+		}
+	})
 
 	function set_theme(event: Event) {
 		const select = event.target as HTMLSelectElement
 		const theme = select.value
 		if (themes.includes(theme)) {
 			const one_year = 60 * 60 * 24 * 365
+			window.localStorage.setItem('theme', theme);
 			document.cookie = `theme=${theme}; max-age=${one_year}; path=/`
 			document.documentElement.setAttribute('data-theme', theme)
+			current_theme = theme;
 		}
 	}
 </script>
 
 <div class="mb-8">
-	<select
+	<select bind:value={current_theme}
 		data-choose-theme
 		class="select select-bordered select-primary w-full max-w-3xl text-xl capitalize"
 		on:change={set_theme}
 	>
-		<option disabled selected>Choose a theme</option>
+		<option disabled={current_theme !== ''}>Choose a theme</option>
 		{#each themes as theme}
 			<option value={theme} class="capitalize">{theme}</option>
 		{/each}
