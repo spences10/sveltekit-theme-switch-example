2dabbe9a0a3524ac0ddc94bf31372012631a3427
spences10 <spences10apps@gmail.com>
2025-02-10 09:09:35 +0000

    feat: :sparkles: use Tailwind v4 and daisyUI v5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import '../app.pcss';
-
-	const { children } = $props();
+	import '../app.css';
+	let { children } = $props();
 </script>
 
 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-	<div class="prose mx-auto max-w-3xl md:prose-lg lg:prose-xl">
+	<div class="prose md:prose-lg lg:prose-xl mx-auto max-w-3xl">
 		<main>
 			{@render children()}
 		</main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
+
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
+		workspace: [
+			{
+				extends: './vite.config.ts',
+				plugins: [svelteTesting()],
+
+				test: {
+					name: 'client',
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts'],
+				},
+			},
+			{
+				extends: './vite.config.ts',
+
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+				},
+			},
+		],
 	},
 });
